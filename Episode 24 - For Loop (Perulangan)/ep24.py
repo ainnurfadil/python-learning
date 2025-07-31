6f4cab40616252c30526648fc450a4dd9b13f717
Mucha (Ainnur Fadil) <fadilainnur@gmail.com>
2025-07-31 08:48:32 +0700

    Update 31 Jul

--- Episode 24 - For Loop (Perulangan)/ep24.py
+++ Episode 24 - For Loop (Perulangan)/ep24.py
@@ -16,21 +16,21 @@ print("akhir dari program 1")
 
 # 3. Looping dengan range() (Dasar)
 print("\n--- Looping dengan range() ---")
-for i in range(5): # range(5) menghasilkan 0, 1, 2, 3, 4
+for i in range(5): 
     print(f"i sekarang -> {i}")
-print("akhir dari program 2")
+print("akhir dari program 2")       # range(5) menghasilkan 0, 1, 2, 3, 4
 
 # 4. Looping dengan range() (Dengan Batas Awal dan Akhir)
 print("\n--- Looping dengan range(start, stop) ---")
-for i in range(1, 5): # range(1, 5) menghasilkan 1, 2, 3, 4
-    print(f"i sekarang -> {i}")
+for i in range(1, 5):              
+    print(f"i sekarang -> {i}")     # range(1, 5) menghasilkan 1, 2, 3, 4
 
 print("\n--- Looping dengan range(1, 10) ---")
-for i in range(1, 10): # range(1, 10) menghasilkan 1, 2, ..., 9
-    print(f"i sekarang -> {i}")
+for i in range(1, 10): 
+    print(f"i sekarang -> {i}")     # range(1, 10) menghasilkan 1, 2, ..., 9
 
 # 5. Looping dengan String
 print("\n--- Looping dengan String ---")
-data_str = "Saya ganteng abis"
+data_str = "Ini looping string"
 for huruf in data_str:
     print(huruf)
